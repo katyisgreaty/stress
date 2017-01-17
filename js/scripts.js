632d2d7fd71a0d4634d3e79ad323c91869de46ce
Cassie Musolf and Katy Daviscourt <katy@>
2017-01-17 13:38:53 -0800

    finish stress test with if else statements in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 var stress = 0;
 
 $(document).ready(function(){
+  $("form#stress-test").submit(function(event){
   event.preventDefault();
-  $(#results)
-})
+ $("input:checkbox[name=warningsigns]:checked").each(function(){
+   var warnings = $(this).val();
+   stress += 1;
+ });
+
+ $("input:checkbox[name=health]:checked").each(function(){
+   var negHealth = $(this).val();
+   stress += 1;
+ });
+
+ $("input:checkbox[name=coping]:checked").each(function(){
+   var copingMethod = $(this).val();
+   stress -= 1;
+ });
+
+
+  if (stress <= 4) {
+    $("#light-results").show();
+  } else if (stress >= 5 && stress <=8) {
+    $("#med-results").show();
+  } else {
+    $("#heavy-results").show();
+  };
+});
+});
